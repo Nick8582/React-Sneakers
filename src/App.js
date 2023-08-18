4a7afee1b66a17da35aa14b42482baaac98ecf30
Vladislav Potapov <89172111013q@gmail.com>
2023-08-18 15:03:54 +0300

    create: card module scss and passing parameters to the component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import Card from "./components/Card";
+import Index from "./components/Card";
 import Header from "./components/Header";
 import Drawer from "./components/Drawer";
+
+const arr = [
+  {title: 'Мужские Кроссовки Nike Blazer Mid Suede', price: 12999, imageUrl: '/img/sneakers/1.jpg'},
+  {title: 'Мужские Кроссовки Nike Air Max 270', price: 12999, imageUrl: '/img/sneakers/2.jpg'},
+  {title: 'Мужские Кроссовки Nike Blazer Mid Suede', price: 8499, imageUrl: '/img/sneakers/3.jpg'},
+  {title: 'Кроссовки Puma X Aka Boku Future Rider', price: 8999, imageUrl: '/img/sneakers/4.jpg'},
+]
 
 function App() {
   return (
     <div className="wrapper clear">
-      <Drawer />
-      <Header />
+      <Drawer/>
+      <Header/>
       <div className="content p-40">
         <div className="d-flex justify-between align-center mb-40">
           <h1>Все кроссовки</h1>
@@ -16,8 +23,11 @@ function App() {
           </div>
         </div>
         <div className="d-flex flex-wrap">
-
-          <Card />
+          {
+            arr.map((obj, index) => (
+              <Index title={obj.title} price={obj.price} imageUrl={obj.imageUrl} onClick={() => console.log(obj)} key={index}/>
+            ))
+          }
         </div>
       </div>
     </div>);
