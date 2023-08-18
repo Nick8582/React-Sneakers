b51de9c48cbba519faa260bebafaa39ef4618eb2
Vladislav Potapov <89172111013q@gmail.com>
2023-08-18 21:57:40 +0300

    create: product search

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import React from "react";
 function App() {
   const [items, setItems] = React.useState([])
   const [cartItems, setCartItems] = React.useState([])
+  const [searchValue, setSearchValue] = React.useState('')
   const [cartOpened, setCartOpened] = React.useState(false)
 
   React.useEffect(() => {
@@ -22,21 +23,33 @@ function App() {
     setCartItems(prev => [...prev, obj])
   }
 
+  const onChangeSearchInput = (event) => {
+    setSearchValue(event.target.value)
+  }
+
   return (
     <div className="wrapper clear">
       {cartOpened && <Drawer items={cartItems} onClose={() => setCartOpened(false)}/>}
       <Header onClickCart={() => setCartOpened(true)}/>
       <div className="content p-40">
         <div className="d-flex justify-between align-center mb-40">
-          <h1>Все кроссовки</h1>
+          <h1>{searchValue ? `Поиск по запросу: "${searchValue}"` : 'Все кроссовки'}</h1>
           <div className="search-block d-flex">
             <img src="/img/search.svg" alt="Search"/>
-            <input type="text" placeholder="Поиск..."/>
+            {searchValue &&
+              <img
+                onClick={() => setSearchValue('')}
+                className="clear cu-p"
+                src="/img/btn-remove.svg" alt="Clear"
+              />}
+            <input onChange={onChangeSearchInput} value={searchValue} type="text" placeholder="Поиск..."/>
           </div>
         </div>
         <div className="d-flex flex-wrap">
-          {
-            items.map((item, index) => (
+          {items
+            .filter(item => item.title.toLowerCase()
+            .includes(searchValue.toLowerCase()))
+            .map((item, index) => (
               <Card
                 title={item.title}
                 price={item.price}
@@ -45,8 +58,7 @@ function App() {
                 onPlus={(obj) => onAddToCart(obj)}
                 key={index}
               />
-            ))
-          }
+            ))}
         </div>
       </div>
     </div>);
