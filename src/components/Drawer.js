2705e713e2ccaca15ed1c6c8b16619028cf620ce
Vladislav Potapov <89172111013q@gmail.com>
2023-08-18 20:21:31 +0300

    add: adding products to the cart

--- src/components/Drawer.js
+++ src/components/Drawer.js
@@ -1,36 +1,24 @@
-function Drawer(props) {
+function Drawer({onClose, items = []}) {
   return (
     <div  className="overlay">
       <div className="drawer">
         <h2 className="d-flex justify-between mb-30 ">
           Корзина
-          <img className="removeBtn cu-p" src="/img/btn-remove.svg" onClick={props.onClose} alt="Close"/>
+          <img className="removeBtn cu-p" src="/img/btn-remove.svg" onClick={onClose} alt="Close"/>
         </h2>
         <div className="items">
-          <div className="cartItem d-flex align-center mb-20">
-            <div style={{backgroundImage: "url('/img/sneakers/1.jpg')"}} className="cartItemImg"></div>
-            <div className="mr-20 flex">
-              <p className="mb-5">Мужские Кроссовки Nike Air Max 270</p>
-              <b>12 999 руб.</b>
-            </div>
-            <img className="removeBtn" src="/img/btn-remove.svg" alt="Remove"/>
-          </div>
-          <div className="cartItem d-flex align-center mb-20">
-            <div style={{backgroundImage: "url('/img/sneakers/1.jpg')"}} className="cartItemImg"></div>
-            <div className="mr-20 flex">
-              <p className="mb-5">Мужские Кроссовки Nike Air Max 270</p>
-              <b>12 999 руб.</b>
-            </div>
-            <img className="removeBtn" src="/img/btn-remove.svg" alt="Remove"/>
-          </div>
-          <div className="cartItem d-flex align-center mb-20">
-            <div style={{backgroundImage: "url('/img/sneakers/1.jpg')"}} className="cartItemImg"></div>
-            <div className="mr-20 flex">
-              <p className="mb-5">Мужские Кроссовки Nike Air Max 270</p>
-              <b>12 999 руб.</b>
-            </div>
-            <img className="removeBtn" src="/img/btn-remove.svg" alt="Remove"/>
-          </div>
+          {
+            items.map((obj, index) => (
+              <div className="cartItem d-flex align-center mb-20" key={index}>
+                <div style={{backgroundImage: `url(${obj.imageUrl})`}} className="cartItemImg"></div>
+                <div className="mr-20 flex">
+                  <p className="mb-5">{obj.title}</p>
+                  <b>{obj.price} руб.</b>
+                </div>
+                <img className="removeBtn" src="/img/btn-remove.svg" alt="Remove"/>
+              </div>
+            ))
+          }
         </div>
         <div className="cartTotalBlock">
           <ul>
